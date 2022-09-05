669193b629361760217b4ea71e96b9e97b494ec9
Bình An <binhan29122003@gmail.com>
2022-09-05 10:56:51 +0700

    Alpha 1.0.4
    Rename Directory

---
+++ AChat/src/ObjectClass/URLPanel.java
@@ -0,0 +1,30 @@
+package ObjectClass; //Nerver use
+// Make By Bình An || AnLaVN || KatoVN
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+
+public class URLPanel extends javax.swing.JPanel{
+//    private BufferedImage image;    private int wi, he = 0; //width and height
+//    public URLPanel(URL URLFile, int w, int h) {
+//        wi = w; he = h;
+//        try { image = ImageIO.read(URLFile);}
+//        catch (IOException ex) {}  
+//    }
+//    @Override protected void paintComponent(Graphics g) {
+//        super.paintComponent(g);
+//        Graphics2D g2 = (Graphics2D) g;
+//        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+//        int circleDiameter = Math.min(getWidth(),getHeight());
+//        Ellipse2D.Double circle = new Ellipse2D.Double(0,0,circleDiameter,circleDiameter);
+//        g2.setClip(circle);
+//        g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+//    }
+//    @Override public Dimension getPreferredSize() { return new Dimension(wi, he); }
+}
