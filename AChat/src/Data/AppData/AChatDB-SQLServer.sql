c6bd61cbe00e4a2a60d0f03a0e1d9e400d23d702
Bình An <binhan29122003@gmail.com>
2022-09-05 22:30:58 +0700

    Add files via upload

--- AChat/src/Data/AppData/AChatDB-SQLServer.sql
+++ AChat/src/Data/AppData/AChatDB-SQLServer.sql
@@ -13,13 +13,14 @@ CREATE TABLE USERINFO(
 	Avatar	 VARCHAR(255)
 );
 CREATE TABLE CHATSQUEUE(
-	PortID CHAR(4) PRIMARY KEY,
-	HostUser CHAR(64),
-	FOREIGN KEY (HostUser) REFERENCES USERINFO(Username)
+	PortID INT PRIMARY KEY,
+	IPAddress VARCHAR(15),
+	Available BIT NOT NULL -- 1 that mean available for connect, 0 that mean was had connect
+	--HostUser CHAR(64),
+	--FOREIGN KEY (HostUser) REFERENCES USERINFO(Username)
 );
 INSERT INTO USERINFO VALUES
 	('67e571ffc1d92b62295a6f15b6f180996179cf967d75d3cfd2d30d11d2f5ce71', 'e86f78a8a3caf0b60d8e74e5942aa6d86dc150cd3c03338aef25b7d2d7e3acc7',	N'Bình An', 'binhan2003@gmail.com', '	http://res.cloudinary.com/anlavn/image/upload/v1661996159/AChat/Avatar/tom_ejhiqc.jpg')
 
 select * from USERINFO
-delete from CHATSQUEUE where PortID = '1234'
 select * from CHATSQUEUE
