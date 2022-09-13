82769c0257abc80e5f6f19ad8e426a5359e1aea2
Bình An <binhan29122003@gmail.com>
2022-09-13 22:33:43 +0700

    Add files via upload

---
+++ AChat/src/Data/AppData/AChatDB-SQLServer.sql
@@ -0,0 +1,23 @@
+USE MASTER
+GO
+
+DROP DATABASE IF EXISTS AChatDB;
+CREATE DATABASE AChatDB;
+USE AChatDB;
+
+CREATE TABLE USERINFO(
+	Username CHAR(64) PRIMARY KEY,
+	Password CHAR(64),
+	Name	 NVARCHAR(255),
+	Email	 VARCHAR(254),
+	Avatar	 VARCHAR(255)
+);
+CREATE TABLE CHATSQUEUE(
+	PortID INT PRIMARY KEY,
+	IPAddress VARCHAR(15),
+	Available BIT NOT NULL -- 1 that mean available for connect, 0 that mean was had connect
+	--HostUser CHAR(64),
+	--FOREIGN KEY (HostUser) REFERENCES USERINFO(Username)
+);
+INSERT INTO USERINFO VALUES
+	('67e571ffc1d92b62295a6f15b6f180996179cf967d75d3cfd2d30d11d2f5ce71', 'e86f78a8a3caf0b60d8e74e5942aa6d86dc150cd3c03338aef25b7d2d7e3acc7',	N'Bình An', 'binhan2003@gmail.com', '	http://res.cloudinary.com/anlavn/image/upload/v1661996159/AChat/Avatar/tom_ejhiqc.jpg')
